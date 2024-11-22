ee03a2cc2141bfa247c286654f11ff1f1c04606e
Cristhian Zeas <31786518+0xSoul24@users.noreply.github.com>
2024-11-22 15:50:23 -0500

    floral: Initialize for Evolution X 10.x

---
+++ coral/device-evolution.mk
@@ -0,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: 2021-2024 The Evolution X Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/google/coral/device-evolution.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/google/coral/coral/overlay-evolution
