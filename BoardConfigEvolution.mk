1788b6d8c2f833dfbc763d8761fe0ec53695933d
0xSoul <31786518+0xSoul24@users.noreply.github.com>
2024-11-23 22:50:45 -0500

    Evolution fixes

--- BoardConfigEvolution.mk
+++ BoardConfigEvolution.mk
@@ -2,3 +2,6 @@
 # SPDX-FileCopyrightText: 2024 The Evolution X Project
 # SPDX-License-Identifier: Apache-2.0
 #
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
